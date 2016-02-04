528027bb963589f9b4f8123b34fddec3691a470a
florent <florent@10.102.39.5>
2016-02-04 10:31:17 +0100

    map

--- VendingMachine/src/vendingmachine/components/ChangeMachine.java
+++ VendingMachine/src/vendingmachine/components/ChangeMachine.java
@@ -12,14 +12,46 @@ public class ChangeMachine {
 	public static final String[] COINS_TEXT = {"2 €", "1 €", 
 		"0,5 €", "0,2 €", "0,1 €", "0,05 €", "0,02 €", "0,01 €"};
 
-	public ChangeMachine(Hashtable<Coin, Integer> coinsStock, Hashtable<Coin, Boolean> acceptedCoins) {
+	public ChangeMachine(Map<Coin, Integer> coinsStock, Map<Coin, Boolean> acceptedCoins) {
 		this.coinsStock = coinsStock;
 		this.acceptedCoins = acceptedCoins;
 	}
 
-	public void giveChange() {
-		// TODO - implement ChangeMachine.giveChange
-		
+	public byte giveChange() {
+
+			long[] coinsStockTemp = coinsStock;
+			long[] moneyToGive = {0,0,0,0,0,0,0,0};
+			for(int i = 0; i < COINS.length; i++) {
+
+	            while(amount >= COINS[i] && coinsStockTemp[i] > 0) {	
+
+	                coinsStockTemp[i] -= 1;
+
+	                amount -= COINS[i];
+
+	                moneyToGive[i] += 1;
+
+	            }	
+
+			}		
+
+			if (amount == 0) {
+
+				coinsStock = coinsStockTemp;
+
+				return moneyToGive; 
+
+			}
+
+			else {
+
+				long[] tab = {-1};	
+
+				return tab;
+
+			}
+
+		}
 	}
 
 	public boolean isCoinAccepted(Coin coin) {
