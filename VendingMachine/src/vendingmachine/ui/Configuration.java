6e942b7738d9b2184331cc1c6bf75ffeabd00992
Pierre <pierrevdh31@hotmail.com>
2016-02-13 00:49:46 +0100

    Factorized code in Configuration

--- VendingMachine/src/vendingmachine/ui/Configuration.java
+++ VendingMachine/src/vendingmachine/ui/Configuration.java
@@ -28,7 +28,7 @@ import vendingmachine.components.Stock;
 public class Configuration extends JFrame {
 
   private static final long serialVersionUID = 1L;
-  
+
   private static final String[] COLUMNS_TITLES = { "Enter here the drinks names: ", "Contains sugar?",
     "Price (in cents): ", "Initial stock:" };
   private static final String[] COINS_TITLES = { "Coins and their values: ", "Initial stock: ",
@@ -38,10 +38,13 @@ public class Configuration extends JFrame {
   private static final Integer[] NBR_DRINKS_LIST = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
   private JPanel myPanel;
-  
+  private JScrollPane scrPane;
+
+  private MyDocumentFilter myDocumentFilter;
+
   private JLabel drinkNbrLabel; //Pas d'instance ?
   private JComboBox<Integer> drinkNbrComboBox;
-  
+
   private JTextField[] drinksNames;
   private JCheckBox[] drinksSugar;
   private JTextField[] drinksPrices;
@@ -66,31 +69,40 @@ public class Configuration extends JFrame {
   public Configuration() {
     super();
     setTitle("Vending Machine Initialization");
-    
+
     drinkNbrComboBox = new JComboBox<Integer>(NBR_DRINKS_LIST);
     drinkNbrComboBox.setSelectedIndex(7); //to have 8 drinks as a default value
     drinkNbrComboBox.addActionListener(e -> again());
-    
+
+    myDocumentFilter = new MyDocumentFilter();
+
     drinkNbrLabel = new JLabel("Number of drinks (between 1 and 10): ");
     sugarCubesNbrLabel = new JLabel("Number of sugar cubes available: ");
     cupsNbrLabel = new JLabel("Number of cups available: ");
     spoonsNbrLabel = new JLabel("Number of spoons availables: ");
+    sugarCubesNbrValue = new JTextField("20", 4);
+    ((AbstractDocument)sugarCubesNbrValue.getDocument()).setDocumentFilter(myDocumentFilter);
+    cupsNbrValue = new JTextField("10", 4);
+    ((AbstractDocument)cupsNbrValue.getDocument()).setDocumentFilter(myDocumentFilter);
+    spoonsNbrValue = new JTextField("8", 4);
+    ((AbstractDocument)spoonsNbrValue.getDocument()).setDocumentFilter(myDocumentFilter);
     problemLabel = new JLabel();
     problemLabel.setForeground(Color.RED);
     createButton = new JButton("Click here to begin the simulation!");
     createButton.addActionListener(e -> check());
+    coinsStockValues = new JTextField[ChangeMachine.COINS.length];
+    acceptedCoinsBoxes = new JCheckBox[ChangeMachine.COINS.length];
   }
-  
+
   private void again() {
     getContentPane().removeAll();
     init();
   }
-  
+
   public void init() {
     myPanel = new JPanel();
     myPanel.setLayout(new GridBagLayout());
     GridBagConstraints c = new GridBagConstraints();
-    MyDocumentFilter myDocumentFilter = new MyDocumentFilter();
 
     final Integer NBR_DRINKS = (Integer)drinkNbrComboBox.getSelectedItem();
     final int NBR_COINS = ChangeMachine.COINS.length;
@@ -99,14 +111,14 @@ public class Configuration extends JFrame {
     myPanel.add(drinkNbrLabel);
     c.gridx = 1;
     myPanel.add(drinkNbrComboBox);
-    
+
     c.gridy += 1; c.gridx = 0;
     for (int i = 0; i < COLUMNS_TITLES.length; i++) {
       myPanel.add(new JLabel(COLUMNS_TITLES[i]), c);
       c.gridx += 1;
     }
 
-    drinksNames = new JTextField[NBR_DRINKS];
+    drinksNames = new JTextField[NBR_DRINKS]; //A factoriser pour pas le refaire à chaque fois ???
     drinksSugar = new JCheckBox[NBR_DRINKS];
     drinksPrices = new JTextField[NBR_DRINKS];
     drinksStocks = new JTextField[NBR_DRINKS];
@@ -141,8 +153,6 @@ public class Configuration extends JFrame {
       myPanel.add(new JLabel(COINS_TITLES[i]), c);
     }
 
-    coinsStockValues = new JTextField[NBR_COINS];
-    acceptedCoinsBoxes = new JCheckBox[NBR_COINS];
     for (int i = 0; i < NBR_COINS; i++) {
       c.gridy += 1;
       coinsStockValues[i] = new JTextField("5", 4);
@@ -158,13 +168,6 @@ public class Configuration extends JFrame {
       c.gridx = 2;
       myPanel.add(acceptedCoinsBoxes[i], c);
     }
-
-    sugarCubesNbrValue = new JTextField("20", 4);
-    ((AbstractDocument)sugarCubesNbrValue.getDocument()).setDocumentFilter(myDocumentFilter);
-    cupsNbrValue = new JTextField("10", 4);
-    ((AbstractDocument)cupsNbrValue.getDocument()).setDocumentFilter(myDocumentFilter);
-    spoonsNbrValue = new JTextField("8", 4);
-    ((AbstractDocument)spoonsNbrValue.getDocument()).setDocumentFilter(myDocumentFilter);
 
     c.gridy += 1;
     c.gridx = 0;
@@ -192,7 +195,7 @@ public class Configuration extends JFrame {
     myPanel.add(problemLabel, c);
 
     add(myPanel);
-    JScrollPane scrPane = new JScrollPane(myPanel); // makes the frame scrollable in case it is shrunk
+    scrPane = new JScrollPane(myPanel); // makes the frame scrollable in case it is shrunk
     add(scrPane);
 
     myPanel.setBorder(new EmptyBorder(10, 10, 20, 10));
@@ -207,6 +210,9 @@ public class Configuration extends JFrame {
     Map<Drink, Integer> drinkQty = new Hashtable<Drink, Integer>();
     try {
       for (int i = 0; i < (Integer)drinkNbrComboBox.getSelectedItem(); i++) {
+        if (drinksNames[i].getText().equals("")) {
+          throw new NumberFormatException(); //Pas propre, pas la bonne exception ???
+        }
         final Drink d = new Drink(drinksNames[i].getText(), drinksSugar[i].isSelected(),
             Integer.parseInt(drinksPrices[i].getText())); // final ici est suffisant ?
         drinkList.add(d);
@@ -258,5 +264,5 @@ public class Configuration extends JFrame {
   private String getProblemText(String part) {
     return "Error while parsing " + part + " info. Fields can't be empty. Integers can't be larger than 2^31.";
   }
-  
+
 }
