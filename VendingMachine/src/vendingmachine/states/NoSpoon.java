59e03688f1bed655dd73b636ea2222862f304ff0
florent <florent@10.102.37.224>
2016-02-18 09:37:03 +0100

    NoSpoon created

---
+++ VendingMachine/src/vendingmachine/states/NoSpoon.java
@@ -0,0 +1,30 @@
+package vendingmachine.states;
+
+import vendingmachine.components.Context;
+
+public final class NoSpoon extends Problem{
+
+  private static final NoSpoon INSTANCE = new NoSpoon();
+
+  public static NoSpoon getInstance() {
+    return INSTANCE;
+  }
+  
+  
+  // Singleton design pattern
+  private NoSpoon() {}
+  
+  @Override
+  public String getDefaultText (Context c){
+    return "No more spoon. Press Confirm to continue without spoon or Cancel ";
+  }
+  @Override
+  public void cancel (Context c) {
+    super.cancel(c);
+    c.changeState(Idle.getInstance());
+  }
+  public void confirm (Context c){
+    c.setBooSpoon (false);
+    c.changeState(Asking.getInstance());
+  }
+}
