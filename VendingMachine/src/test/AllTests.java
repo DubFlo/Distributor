34331d10b2a5db5c2c763a7b8de8179c4235b9b8
florent <florent@10.102.70.122>
2016-03-19 20:04:38 +0100

    Test

--- VendingMachine/src/test/AllTests.java
+++ VendingMachine/src/test/AllTests.java
@@ -1,4 +1,4 @@
-package vendingmachine.test;
+package test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
--- VendingMachine/src/test/HeatingSystemTest.java
+++ VendingMachine/src/test/HeatingSystemTest.java
@@ -1,14 +1,13 @@
-package vendingmachine.test;
+package test;
 
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import test.EmptyContext;
 import vendingmachine.components.HeatingSystem;
 import vendingmachine.components.IContext;
-import vendingmachine.states.NoWater;
 
 public class HeatingSystemTest {
   
@@ -21,14 +20,14 @@ public class HeatingSystemTest {
   }
   
   @Test
-  public void setTemperatureTest() {
+  public void testSetTemperature() {
       heatSys.setTemperature(70);
       assertEquals(70,heatSys.getTemperature(),1);
       heatSys.setTemperature(100);
       assertEquals(100,heatSys.getTemperature(),0.01); 
   }
   @Test
-  public void setWaterSupplyTest() {
+  public void testSetWaterSupply() {
     //heatSys.waterSupply = true
     heatSys.setWaterSupply(true);
     assertTrue("Nothing should have changed",heatSys.isWaterSupplyEnabled());
@@ -45,9 +44,22 @@ public class HeatingSystemTest {
     heatSys.setWaterSupply(true);
     assertTrue(heatSys.isWaterSupplyEnabled());
   }
+
+  @Ignore
+  public void testUpdate () throws InterruptedException {
+    heatSys.setTemperature(96.1);
+    Thread.sleep(2500);
+    assertEquals (95,heatSys.getTemperature(),0.3);
+    heatSys.setTemperature(110);
+    Thread.sleep(250);
+    assertEquals(110, heatSys.getTemperature(),0.02);
+    
+  }
   @Test
-  public void drinkOrderedTest() {
-    sleep(1);
+  public void testDrinkOrdered() {
+    assertEquals(90.1,heatSys.getTemperature(),0.1);
+    heatSys.drinkOrdered();
+    assertEquals(84.45, heatSys.getTemperature(), 0.1);
   }
 
 }
