d4cec3231634adff4fb58249940446546d66c96e
Pierre <pierrevdh31@hotmail.com>
2016-03-21 19:38:13 +0100

    Classe change linked to changeMachine

--- VendingMachine/src/vendingmachine/components/ChangeMachine.java
+++ VendingMachine/src/vendingmachine/components/ChangeMachine.java
@@ -18,99 +18,50 @@ import vendingmachine.Utils;
  * @see Coin
  */
 public class ChangeMachine {
-
-  private static final Logger log = LogManager.getLogger("ChangeMachine");
-  
-  /**
-   * A Map of the coins stock, mapping each Coin to its stock.
-   */
-  private final Map<Coin, Integer> coinsStock;
   
   /**
    * A Map mapping each Coin to whether or not it is accepted.
    */
   private final Map<Coin, Boolean> acceptedCoins;
   
-  //private final Change change;
-  
-  /**
-   * Used by {@code isChangePossible(int} to use the computation done in
-   * {@code giveChange(int)}. Can not be accessed from the outside.
-   */
-  private Map<Coin, Integer> coinsStockTemp;
+  private final Change change;
 
   /**
    * Builds a change machine with the specified coins stock.
    * Each coin may be accepted or not by the change machine.
-   * Throws an IllegalArgumentException if a stock value is negative, or if
-   * {@code coinsStock} or {@code acceptedCoins} do not list all the coins of Coin.
+   * Throws an IllegalArgumentException if {@code acceptedCoins}
+   * do not list all the coins of Coin.
    * 
    * @param coinsStock a Map that maps each Coin to its stock value
    * @param acceptedCoins a Map that tells if each Coin is accepted or not
    */
-  public ChangeMachine(Map<Coin, Integer> coinsStock, Map<Coin, Boolean> acceptedCoins) {
-    for (Integer i: coinsStock.values()) {
-      Utils.checkPositiveIntIllegal(i, "stock of coins");
-    }
-    if (!coinsStock.keySet().containsAll(Coin.COINS)) {
-      throw new IllegalArgumentException("coinsStock has to list all the coins defined in Coin");
-    }
+  public ChangeMachine(Change change, Map<Coin, Boolean> acceptedCoins) {
     if (!acceptedCoins.keySet().containsAll(Coin.COINS)) {
       throw new IllegalArgumentException("acceptedCoins has to list all the coins defined in Coin");
     }
-    //change = new Change(coinsStock, acceptedCoins);
-    this.coinsStock = coinsStock;
+    this.change = change;
     this.acceptedCoins = acceptedCoins;
-    
-    this.coinsStockTemp = new Hashtable<Coin, Integer>();
   }
 
   /**
-   * Gives change on the amount specified and updates the coins stock accordingly.
-   * If it is not possible, throws an IllegalArgumentException.
-   * Returns a Map of the Coin's given back (mapping each Coin to the number of times
-   * it is given). Updates the specified IContext of the coins that are given back.
+   * Gives change on the specified amount.
    * 
    * @param amount the amount to give change on
    * @param context the IContext to notify of the coins given
-   * @return a Map of the money that is given back.
    */
-  public Map<Coin, Integer> giveChange(int amount, IContext context) {
-    if (!isChangePossible(amount)) {
-      throw new IllegalArgumentException();
-    }
-    final Map<Coin, Integer> moneyToGive = new Hashtable<Coin, Integer>();
-    for (Coin coin: Coin.COINS) {
-      moneyToGive.put(coin, coinsStock.get(coin) - coinsStockTemp.get(coin));
-      coinsStock.put(coin, coinsStockTemp.get(coin));
-    }
-    context.addChangeOut(moneyToGive);
-    return moneyToGive;
+  public void giveChange(int amount, IContext context) {
+    context.addChangeOut(change.giveChange(amount));
   }
 
   /**
    * Returns true if it is possible to give change with the current stock
-   * for the amount value (in cents), false otherwise. Updates coinsStockTemp 
-   * accordingly, which is the new value coinsStock should take after giving the change.
-   * Throws an IllegalArgumentException if {@code amount} is negative.
+   * for the amount value (in cents), false otherwise.
    * 
    * @param amount number of cents to give change for.
    * @return true if change on the amount is possible, false otherwise
    */
   public boolean isChangePossible(int amount) {
-    Utils.checkPositiveIntIllegal(amount, "the amount to give change on");
-    coinsStockTemp = Utils.copy(coinsStock);
-    int remainder = amount;
-    for (Coin coin: Coin.COINS) {
-      while (remainder >= coin.VALUE && coinsStockTemp.get(coin) > 0) {
-        coinsStockTemp.put(coin, coinsStockTemp.get(coin) - 1);
-        remainder -= coin.VALUE;
-      }
-    }
-    if (remainder != 0) {
-      log.warn("Can not give " + amount / 100.0 + " " + Utils.EURO + " of change.");
-    }
-    return remainder == 0;
+    return change.isChangePossible(amount);
   }
 
   /**
@@ -119,7 +70,7 @@ public class ChangeMachine {
    * @param coin the Coin to add to the stock
    */
   public void insertCoin(Coin coin) {
-    coinsStock.put(coin, coinsStock.get(coin) + 1);
+    change.insertCoin(coin);
   }
 
   /**
@@ -135,7 +86,7 @@ public class ChangeMachine {
    * @return the stock value of the specified Coin
    */
   public int getCoinsStock(Coin coin) {
-    return coinsStock.get(coin);
+    return change.getCoinsStock(coin);
   }
 
   /**
@@ -146,7 +97,7 @@ public class ChangeMachine {
     sb.append("Coins:\n");
     for (Coin coin: Coin.COINS) {
       sb.append(coin.TEXT).append(": ")
-      .append(coinsStock.get(coin))
+      .append(change.getCoinsStock(coin))
       .append(" available.\n");
     }
     return sb.toString();
@@ -154,16 +105,12 @@ public class ChangeMachine {
 
   /**
    * Updates the stock of {@code coin} to the {@code value} specified.
-   * Throws an IllegalArgumentException if the value is negative.
    * 
    * @param coin the Coin whose stock must be changed
    * @param value the new value for the {@code coin} (must be positive)
    */
   public void setCoinStock(Coin coin, int value) {
-    Utils.checkPositiveIntIllegal(value, "stock of " + coin.TEXT + " coins");
-    Utils.logChange(value - coinsStock.get(coin), value, "\"" + coin.TEXT + "\" coin(s)");
-    
-    coinsStock.put(coin, value);
+    change.setCoinStock(coin, value);
   }
   
 }
